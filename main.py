c1b1573c6a29ef5eeffa17edc4e09fd49b1f8ac1
Datdynohat <93474136+Datdynohat@users.noreply.github.com>
2023-12-02 00:33:07 +0100

    new very advanced and super cool submissions feature :D

--- main.py
+++ main.py
@@ -155,11 +155,16 @@ async def add_level(lvl: str):
 
 @bot.event
 async def on_application_command_error(ctx, error):
+    log(error)
     print(error)
     try:
         await ctx.send(f"{fwogutils.errormessage(error)}", ephemeral=True)
     except nextcord.errors.InteractionResponded:
         pass
+
+@bot.event
+async def on_command_error(ctx, error):
+    log(error)
 
 @bot.event
 async def on_ready():
@@ -728,5 +733,50 @@ async def fetchmessages(data: dict):
         await channel.send(fwogutils.errormessage(ewwor))
         log(ewwor)
 
+emb = nextcord.InteractionResponse
+page = {"page": 1, "limit": 10, "offset": 0}
+@bot.slash_command(name="rankings")
+async def rankings(ctx):
+    global emb, page
+    page = {"page": 1, "limit": 10, "offset": 0}
+    class Lbpage(nextcord.ui.View):
+        def __init__(self):
+            super().__init__(timeout=30)
+            self.page = page
+        @nextcord.ui.button(label="🡨", style=nextcord.ButtonStyle.blurple)
+        async def left(self, ctx: nextcord.Interaction, button: nextcord.Button):
+            if self.page['page'] > 1:
+                self.page['limit'] -= 10
+                self.page['offset'] -= 10
+                self.page['page'] -= 1
+                lb = json.loads(requests.get(f"https://api.zeepkist-gtr.com/users/rankings?Limit={self.page['limit']}&Offset={self.page['offset']}").text)
+                embed = discord.Embed(title="GTR Rankings", color=nextcord.Color.purple(), timestamp=datetime.datetime.now())
+                embed = discord.Embed.set_footer(self=embed, text=f"page: {page['page']}, Total amount: {lb['totalAmount']}")
+                for x in lb["rankings"]:
+                    embed.add_field(name=f"{x['position']}. {x['user']['steamName']}", inline=False,
+                                    value=f"World Records: {x['amountOfWorldRecords']}\nScore: {x['score']}")
+                await emb.edit(embed=embed, view=Lbpage())
+        @nextcord.ui.button(label="🡪", style=nextcord.ButtonStyle.blurple)
+        async def right(self, ctx: nextcord.Interaction, button: nextcord.Button):
+            self.page['limit'] += 10
+            self.page['offset'] += 10
+            self.page['page'] += 1
+            lb = json.loads(requests.get(f"https://api.zeepkist-gtr.com/users/rankings?Limit={self.page['limit']}&Offset={self.page['offset']}").text)
+            embed = discord.Embed(title="GTR Rankings", color=nextcord.Color.purple(), timestamp=datetime.datetime.now())
+            embed = discord.Embed.set_footer(self=embed, text=f"page: {page['page']}, Total amount: {lb['totalAmount']}")
+            for x in lb["rankings"]:
+                embed.add_field(name=f"{x['position']}. {x['user']['steamName']}", inline=False,
+                                value=f"World Records: {x['amountOfWorldRecords']}\nScore: {x['score']}")
+            await emb.edit(embed=embed, view=Lbpage())
 
-bot.run(privaat.token)
+    lbb = json.loads(requests.get("https://api.zeepkist-gtr.com/users/rankings?Limit=10&Offset=0").text)
+    embedd = discord.Embed(title="GTR Rankings", color=nextcord.Color.purple(), timestamp=datetime.datetime.now())
+    embedd = discord.Embed.set_footer(self=embedd, text=f"page: {page['page']}, Total amount: {lbb['totalAmount']}")
+    for x in lbb["rankings"]:
+        embedd.add_field(name=f"{x['position']}. {x['user']['steamName']}", inline=False,
+                        value=f"World Records: {x['amountOfWorldRecords']}\nScore: {x['score']}")
+    emb = await ctx.send(embed=embedd, view=Lbpage())
+
+
+
+bot.run(privaat.ttoken)
