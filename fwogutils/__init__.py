0cc979534a9a40324773ec0238be824f8a7eda9c
fwogiie <geennaamacount@gmail.com>
2024-02-27 22:07:55 +0100

    Updated loop schedule for the rankings func

--- fwogutils/__init__.py
+++ fwogutils/__init__.py
@@ -173,16 +173,13 @@ def get_linked_users():
         return data['linked']
 
 def all_24hours():
-    return [
-        datetime.time(hour=0, minute=1), datetime.time(hour=1, minute=1), datetime.time(hour=2, minute=1),
-        datetime.time(hour=3, minute=1), datetime.time(hour=4, minute=1), datetime.time(hour=5, minute=1),
-        datetime.time(hour=6, minute=1), datetime.time(hour=7, minute=1), datetime.time(hour=8, minute=1),
-        datetime.time(hour=9, minute=1), datetime.time(hour=10, minute=1), datetime.time(hour=11, minute=1),
-        datetime.time(hour=12, minute=1), datetime.time(hour=13, minute=1), datetime.time(hour=14, minute=1),
-        datetime.time(hour=15, minute=1), datetime.time(hour=16, minute=1), datetime.time(hour=17, minute=1),
-        datetime.time(hour=18, minute=1), datetime.time(hour=19, minute=1), datetime.time(hour=20, minute=1),
-        datetime.time(hour=21, minute=1), datetime.time(hour=22, minute=1), datetime.time(hour=23, minute=1)
-    ]
+    times = []
+    for x in range(24):
+        times.append(datetime.time(hour=x, minute=10))
+        times.append(datetime.time(hour=x, minute=25))
+        times.append(datetime.time(hour=x, minute=40))
+        times.append(datetime.time(hour=x, minute=55))
+    return times
 
 def setlinkedusersetting(setting: str, value, user):
     with open("users.json", 'r') as f:
