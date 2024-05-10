569bd9475b8e79e7093fc3a50ffad45b79adcb23
Fwogiie <geennaamacount@gmail.com>
2024-05-10 21:06:23 +0200

    Showdown stuff

--- main.py
+++ main.py
@@ -1559,27 +1559,30 @@ async def sd_register(ctx):
                        f"**By registering you accept that we need to gather your steam ID, your steam Name and your GTR user ID for organizational and technical reasons**", ephemeral=True, view=agreebtns)
     else:
         user = fwogutils.userhandler(discordid=str(ctx.user.id))
-        async def btn_yes(ctx: nextcord.Interaction):
-            newuser = {"id": user['id'], "steamId": user['steamId'], "steamName": user['steamName'], "discordId": user['discordId'], "registered": True}
-            sdusers["s4"].append(newuser)
-            sdusers["registered_users"].append(int(ctx.user.id))
-            with open("showdownusers.json", 'w') as write:
-                json.dump(sdusers, write, indent=2)
-            requests.post("https://zeepkist-showdown-4215308f4ce4.herokuapp.com/api/v2/qualifier/register", json=newuser)
-            await ctx.edit(content="Thank you for participating!", view=None)
-            await ctx.channel.send(f"{ctx.user.mention} has joined the competition!")
-        async def btn_no(ctx: nextcord.Interaction):
-            await ctx.edit(content="Alrighty!", view=None)
-            return
-        agreebtns = nextcord.ui.View(timeout=60)
-        btnyes = nextcord.ui.Button(label="Yes", style=nextcord.ButtonStyle.green)
-        btnyes.callback = btn_yes
-        btnno = nextcord.ui.Button(label="No", style=nextcord.ButtonStyle.red)
-        btnno.callback = btn_no
-        agreebtns.add_item(btnyes)
-        agreebtns.add_item(btnno)
-        await ctx.send(f"I have found a steam account by the name of **{user['steamName']}**\nDo you wish to register?\n\n"
-                       f"**By registering you accept that we need to gather your steam ID, your steam Name and your GTR user ID for organizational and technical reasons**", ephemeral=True, view=agreebtns)
+        if user != False:
+            async def btn_yes(ctx: nextcord.Interaction):
+                newuser = {"id": user['id'], "steamId": user['steamId'], "steamName": user['steamName'], "discordId": user['discordId'], "registered": True}
+                sdusers["s4"].append(newuser)
+                sdusers["registered_users"].append(int(ctx.user.id))
+                with open("showdownusers.json", 'w') as write:
+                    json.dump(sdusers, write, indent=2)
+                requests.post("https://zeepkist-showdown-4215308f4ce4.herokuapp.com/api/v2/qualifier/register", json=newuser)
+                await ctx.edit(content="Thank you for participating!", view=None)
+                await ctx.channel.send(f"{ctx.user.mention} has joined the competition!")
+            async def btn_no(ctx: nextcord.Interaction):
+                await ctx.edit(content="Alrighty!", view=None)
+                return
+            agreebtns = nextcord.ui.View(timeout=60)
+            btnyes = nextcord.ui.Button(label="Yes", style=nextcord.ButtonStyle.green)
+            btnyes.callback = btn_yes
+            btnno = nextcord.ui.Button(label="No", style=nextcord.ButtonStyle.red)
+            btnno.callback = btn_no
+            agreebtns.add_item(btnyes)
+            agreebtns.add_item(btnno)
+            await ctx.send(f"I have found a steam account by the name of **{user['steamName']}**\nDo you wish to register?\n\n"
+                           f"**By registering you accept that we need to gather your steam ID, your steam Name and your GTR user ID for organizational and technical reasons**", ephemeral=True, view=agreebtns)
+        else:
+            await ctx.send("You do not have your discord linked to GTR! to proceed please go to your mods settings in the game settings, scrolling to the GTR section, in the Discord section press 'link'", ephemeral=True)
 
 
 @bot.slash_command(name="unregister", description="Unregister from the Showdown Competition", guild_ids=[1127321762686836798])
