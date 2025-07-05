300e1de57c9f74e4502851abf3ad2d65f8a7b704
Naomi <geennaamacount@gmail.com>
2025-07-05 15:35:24 +0200

    Fix top GTR playlist generator

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ async def on_ready():
     for guild in bot.guilds:
         log(f"Connected to guild: {guild.name} ({guild.id}) with {guild.member_count} members.")
     log("initializing startup cache for live leaderboards.")
-    # TODO Broken for now: await playlist_stuff.top_gtr.bot_startup_handler()
+    await playlist_stuff.top_gtr.bot_startup_handler()
     await rankings.rankings.startup_handler()
     log("assumed to have begun rankings leaderboard!")
     await startup_logic()
